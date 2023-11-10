9382535646d1d982ccd54d9fed1c759ee0e30cf1
adriano-ms <amaciel_sanchez@hotmail.com>
2023-11-10 11:50:43 -0300

    Adding comments to posts

--- src/main/java/com/adrianoms/workshopmongo/config/Instantiation.java
+++ src/main/java/com/adrianoms/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import com.adrianoms.workshopmongo.domain.Post;
 import com.adrianoms.workshopmongo.domain.User;
 import com.adrianoms.workshopmongo.dto.AuthorDTO;
+import com.adrianoms.workshopmongo.dto.CommentDTO;
 import com.adrianoms.workshopmongo.repository.PostRepository;
 import com.adrianoms.workshopmongo.repository.UserRepository;
 
@@ -39,6 +40,12 @@ public class Instantiation implements CommandLineRunner {
 		Post p3 = new Post(null, Instant.parse("2023-11-01T08:05:01.00Z"), "Job Interview", "Going to my job interview at Microsoft, wish me luck", new AuthorDTO(alex));
 		Post p4 = new Post(null, Instant.parse("2023-10-30T09:10:15.00Z"), "John Birthday", "Today is the birthday of my good friend John. Happy Birthday John!", new AuthorDTO(bob));
 		
+		p1.getComments().add(new CommentDTO("Good travel!", Instant.parse("2023-11-10T10:21:00.00Z"), new AuthorDTO(alex)));
+		p2.getComments().add(new CommentDTO("Nice!", Instant.parse("2023-11-10T12:53:05.00Z"), new AuthorDTO(bob)));
+		p3.getComments().add(new CommentDTO("Good luck, Alex!", Instant.parse("2023-11-01T08:07:01.00Z"), new AuthorDTO(maria)));
+		p4.getComments().add(new CommentDTO("Happy Birthday John!", Instant.parse("2023-10-30T09:32:15.00Z"), new AuthorDTO(alex)));
+		p4.getComments().add(new CommentDTO("I want a big party!", Instant.parse("2023-10-30T09:40:15.00Z"), new AuthorDTO(maria)));
+		
 		postRepository.saveAll(Arrays.asList(p1, p2, p3, p4));		
 	
 		maria.getPosts().addAll(Arrays.asList(p1,p2));
@@ -46,6 +53,5 @@ public class Instantiation implements CommandLineRunner {
 		bob.getPosts().add(p4);
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
-
 	}
 }
